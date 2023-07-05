a4c992ed54d830d77ad02f7e6fd39927a2cafbd6
Коля Бобко <Bobko2604@outlook.com>
2023-07-05 21:04:23 +0300

    update code

--- Project1/Source.c
+++ Project1/Source.c
@@ -7,22 +7,22 @@ int main(void) {
 
     SetConsoleOutputCP(1251);
 
-    // Запитати час для першого гостя (t1) від користувача
+    // Request time for the first guest (t1) from the user
     printf("Enter the time for the first guest (t1): ");
     scanf("%d", &t1);
 
-    // Запитати час для другого гостя (t2) від користувача
+    // Request time for the second guest (t2) from the user
     printf("Enter the time for the second guest (t2): ");
     scanf("%d", &t2);
 
-    // Запитати час для третього гостя (t3) від користувача
+    // Request time for the third guest (t3) from the user
     printf("Enter the time for the third guest (t3): ");
     scanf("%d", &t3);
 
-    // Обчислити загальний час, який займе гостям з'їсти пиріг
+    // Calculate the total time it will take guests to eat the pie
     total_time = 1.0 / t1 + 1.0 / t2 + 1.0 / t3;
 
-    // Вивести час, який займе гостям з'їсти пиріг, округлений до 2 знаків після коми
+    // Print the time it will take the guests to eat the pie, rounded to 2 decimal places
     printf("The time it takes for the guests to eat the cake: %.2f hours\n", 1.0 / total_time);
 
     return 0;
